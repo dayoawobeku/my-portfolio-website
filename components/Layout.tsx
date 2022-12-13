eae6fba6a3f7c9ee115f735cbdf6e035e15c3dcb
Dayo <dayo@dojah.io>
2022-12-13 16:44:45 +0100

    added snowy effect

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -64,6 +64,7 @@ interface Props {
 function Layout({children}: Props) {
   const {theme, setTheme} = useTheme();
   const [menuOpen, setMenuOpen] = useState(false);
+  const [mounted, setMounted] = useState(false);
   const [currentTime, setCurrentTime] = useState(
     new Date().toLocaleTimeString([], {
       hour: '2-digit',
@@ -91,6 +92,61 @@ function Layout({children}: Props) {
     };
   }, []);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  // https://embed.im/snow/
+  function letItSnow() {
+    let embedimSnow = document.getElementById('embedim--snow');
+    function embRand(a: number, b: number) {
+      return Math.floor(Math.random() * (b - a + 1)) + a;
+    }
+    if (embedimSnow) {
+      embedimSnow.remove();
+      return;
+    }
+
+    let embCSS =
+      '.embedim-snow{position: absolute;width: 10px;height: 10px;background: white;border-radius: 50%;margin-top:-10px}.light .embedim-snow{background: #d1edff}';
+    let embHTML = '';
+    for (let i = 1; i < 200; i++) {
+      embHTML += '<i class="embedim-snow"></i>';
+      const rndX = embRand(0, 1000000) * 0.0001;
+      const rndO = embRand(-100000, 100000) * 0.0001;
+      const rndT = (embRand(3, 8) * 10).toFixed(2);
+      const rndS = (embRand(0, 10000) * 0.0001).toFixed(2);
+      embCSS +=
+        `.embedim-snow:nth-child(${i}){` +
+        `opacity:${(embRand(1, 10000) * 0.0001).toFixed(2)};` +
+        `transform:translate(${rndX.toFixed(2)}vw,-10px) scale(${rndS});` +
+        `animation:fall-${i} ${embRand(10, 30)}s -${embRand(
+          0,
+          30,
+        )}s linear infinite` +
+        '}' +
+        `@keyframes fall-${i}{${rndT}%{` +
+        `transform:translate(${(rndX + rndO).toFixed(
+          2,
+        )}vw,${rndT}vh) scale(${rndS})` +
+        '}' +
+        'to{' +
+        `transform:translate(${(rndX + rndO / 2).toFixed(
+          2,
+        )}vw, 105vh) scale(${rndS})` +
+        '}' +
+        '}';
+    }
+    embedimSnow = document.createElement('div');
+    embedimSnow.id = 'embedim--snow';
+    embedimSnow.innerHTML = `<style>#embedim--snow{position:fixed;left:0;top:0;bottom:0;width:100vw;height:100vh;overflow:hidden;z-index:9999999;pointer-events:none}${embCSS}</style>${embHTML}`;
+    document.body.appendChild(embedimSnow);
+  }
+
   return (
     <div className="dark:bg-[#131920]">
       <div className="mx-auto max-w-[1168px] px-4">
@@ -101,7 +157,13 @@ function Layout({children}: Props) {
                 Dayo Awobeku
               </a>
             </Link>
-            {/* <button className="font-medium text-info">snow</button> */}
+            <button
+              onClick={letItSnow}
+              title="Let it snow"
+              className="animate-spin-slow"
+            >
+              ❄️
+            </button>
 
             <div className="flex items-center gap-6 md:gap-8">
               <button
