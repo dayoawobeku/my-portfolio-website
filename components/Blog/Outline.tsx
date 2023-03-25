76fb2125a64786510a4e3064a8b0df7a04cd9e21
Dayo <dayo@dojah.io>
2023-03-25 22:45:17 +0100

    updated outline component

--- components/Blog/Outline.tsx
+++ components/Blog/Outline.tsx
@@ -8,8 +8,12 @@ const Outline = ({children}: Props) => {
   const headings = React.Children.toArray(children)
     .filter(child => React.isValidElement(child) && child.type === 'a')
     .map((anchor, index) => {
-      const id = anchor?.props.href.substring(1);
-      const text = anchor?.props.children;
+      if (!React.isValidElement(anchor)) return null; // Add this check
+      const id = (
+        anchor as React.ReactElement<{href: string}>
+      ).props.href.substring(1);
+      const text = (anchor as React.ReactElement).props
+        .children as React.ReactNode;
       return (
         <li className="my-3 w-fit text-grey-600" key={id}>
           <a
